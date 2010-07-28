1901ef6e74b2edbc028eca22948b880454192fbd
Bala Clark <nandasunu@gmail.com>
2010-07-28 16:21:12 +0100

    color switcher done

--- examples/paint.js
+++ examples/paint.js
@@ -1,13 +1,16 @@
 
+var drawing;
 
 window.onload = function() {
 	
-	var drawing = new CanvasDrawing("canvas"),
-		swatches = document.getElementById("swatch").getElementsByTagName("div");
+	drawing = new CanvasDrawing("canvas");
+	var swatches = document.getElementById("swatches").getElementsByTagName("div");
 
 	// colour the swatches & add click event
 	for (var i=0; i < swatches.length; i++) {
-		swatches[i].addEventListener("click", , false);
-		swatches[i].style["background-color"] = swatches[i].getAttribute("data-color");
+		swatches[i].style.backgroundColor = swatches[i].getAttribute("data-color");
+		swatches[i].addEventListener("click", function() { 
+			drawing.setOption("color", this.getAttribute("data-color"));
+		}, false);
 	}
 };
