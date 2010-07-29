254d6df712d4b32e96f139ac75f2bba129cb05ce
Bala Clark <nandasunu@gmail.com>
2010-07-29 10:34:13 +0100

    very basic (& fugly) paint app example

--- examples/paint.js
+++ examples/paint.js
@@ -1,16 +1,33 @@
-
-var drawing;
-
 window.onload = function() {
 	
-	drawing = new CanvasDrawing("canvas");
-	var swatches = document.getElementById("swatches").getElementsByTagName("div");
+	var i, drawing = new CanvasDrawing("canvas"),
+		swatches = document.getElementById("swatches").getElementsByTagName("div"),
+		brushSizes = document.getElementById("brush-sizes").getElementsByTagName("div");
 
-	// colour the swatches & add click event
-	for (var i=0; i < swatches.length; i++) {
+	// setup controls
+
+	// colour swatches
+	for (i=0; i < swatches.length; i++) {
+
+		// colour the swatches
 		swatches[i].style.backgroundColor = swatches[i].getAttribute("data-color");
-		swatches[i].addEventListener("click", function() { 
+
+		// add controls
+		swatches[i].addEventListener("click", function() {
 			drawing.setOption("color", this.getAttribute("data-color"));
 		}, false);
 	}
+
+	// brush sizes
+	for (i=0; i < 2; i++) {
+		brushSizes[i].addEventListener("click", function() {
+			// set line width
+			drawing.setOption("lineWidth", drawing.options.lineWidth + parseInt(this.getAttribute("data-increment"), 10));
+			// display current line width
+			document.getElementById("brush-size").innerHTML = Math.floor(drawing.options.lineWidth);
+		}, false);
+	}
+
+	// clear
+	document.getElementById("clear").addEventListener("click", drawing.clearCanvas, false);
 };
